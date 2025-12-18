29ea49496ce2276aaf2dbe8c6441f9361e9ea02d
jeongseyoung <sengangel@naver.com>
2025-12-18 23:58:22 +0900

    Update CertificationEntity.java

--- back/src/main/java/com/housing/back/entity/CertificationEntity.java
+++ back/src/main/java/com/housing/back/entity/CertificationEntity.java
@@ -7,7 +7,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Getter
+@Getter 
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity(name = "certification")
