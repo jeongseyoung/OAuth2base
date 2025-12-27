989b3698a7073202c8ddf7c482e6f7d9b927dacd
jeongseyoung <sengangel@naver.com>
2025-12-27 23:42:25 +0900

    Update UserController.java

--- back/src/main/java/com/housing/back/controller/UserController.java
+++ back/src/main/java/com/housing/back/controller/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class UserController {
     // @GetMapping("/${userID}")
     // public String getMethodName(@PathVariable String param) {
-    // return new String();
+    // return new String();     
     // }
 
 }
