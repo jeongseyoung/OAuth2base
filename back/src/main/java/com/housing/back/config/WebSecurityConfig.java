c08d37da9b18dbfcb15f7f74c60a01783a2bbf9f
jeongseyoung <sengangel@naver.com>
2026-01-04 22:18:38 +0900

    Update WebSecurityConfig.java

--- back/src/main/java/com/housing/back/config/WebSecurityConfig.java
+++ back/src/main/java/com/housing/back/config/WebSecurityConfig.java
@@ -18,7 +18,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
+    
 import com.housing.back.filter.JwtAuthenticationFilter;     
 import com.housing.back.handler.OAuth2SuccessHandler;
 
