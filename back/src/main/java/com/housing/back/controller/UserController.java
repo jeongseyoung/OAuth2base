9a7f186d4f9f794c1a9c99f888f12b279602001c
jeongseyoung <sengangel@naver.com>
2026-04-03 23:37:52 +0900

    Update UserController.java

--- back/src/main/java/com/housing/back/controller/UserController.java
+++ back/src/main/java/com/housing/back/controller/UserController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
-
+      
 @RestController
 @RequestMapping("/api/v1/user")
 public class UserController {
